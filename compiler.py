e9b8cdbc3080091346ff64af4b44a4253d2aab0c
mrunhap <liubolovelife@gmail.com>
2019-05-19 12:35:15 +0800

    使用lamba进一步优化 依然缺少负数与小数

--- compiler.py
+++ compiler.py
@@ -2,48 +2,34 @@
 # -*- coding: utf-8 -*-
 import sys, json, os
 from io import StringIO, BytesIO
-from functools import reduce
-
-# 将字符串转换为整形
-DIGITS = {
-    '0': 0, '1': 1, '2': 2, '3': 3, '4': 4,
-    '5': 5, '6': 6, '7': 7, '8': 8, '9': 9
-    }
-def char2num(s):
-    return DIGITS[s]
-def str2int(s):
-    return reduce(lambda x, y: x * 10 + y, map(char2num, s))
-
-# 获取编码文件并序列化为字典
-with open('./complier.json', 'r') as f:
-    SCANER = json.loads(f.read())
-
-"""
-构造一个格式化的字符串: (%d, %s)\n
-"""
-def buildFormatStr(token2code, token):
-    return "(%d, %s)\n"%(SCANER[token2code], token)
 
 """
 对file文件进行词法分析，将结果以格式化的形式写入
 当前目录下的result.txt文件中.
 """
 def scaner(file):
+    # 将字符串转换为整形
+    DIGITS = {
+        '0': 0, '1': 1, '2': 2, '3': 3, '4': 4,
+        '5': 5, '6': 6, '7': 7, '8': 8, '9': 9
+        }
+    # 获取编码文件并序列化为字典
+    with open('./complier.json', 'r') as f:
+        SCANER = json.loads(f.read())
     # 字节流才能使用seek()改变文件指针pos
     with open(file, 'rb') as f:
         _EXAMPLE = BytesIO(f.read())
-    # 每次从file中读取制定字节数
-    getCharFromFile = lambda x=1: (_EXAMPLE.read(x)).decode('utf-8')
-
-    # 如果文件不存在会自动创建
+    # 每次从file中读取制定字节数，如果文件不存在会自动创建
     Result = open('./result.txt', 'at+')
     # 如果文件不为空则删除文件内容
     if os.path.getsize('./result.txt') > 0:
         Result.seek(0)
         Result.truncate()
-
     # result: 内存中用来存放结果的数据流
     _Result = StringIO('')
+    getCharFromFile = lambda size=1: (_EXAMPLE.read(size)).decode('utf-8')
+    buildFormatStr = lambda key, token: "(%d, %s)\n"%(SCANER[key], token)
+
     ch = getCharFromFile()
     while ch:
         # token: 用来连接读取到的字符
@@ -110,5 +96,4 @@ def main():
         scaner(sys.argv[1])
     else:
         print("请以正确格式运行。")
-
 main()
