9af6f8abc21e7108a6e26a449d8c15d14df595f8
mrunhap <liubolovelife@gmail.com>
2019-05-20 18:13:28 +0800

    完成函数 first_vn_from_line

--- test.py
+++ test.py
@@ -15,7 +15,8 @@ def grammer_from_file():
         if line:
             # 去掉末尾\n
             grammer.append(line[:-1])
-        else: break
+        else: 
+            break
     return grammer
 
 """
@@ -34,13 +35,19 @@ def grammer_cut(grammer):
                 if '|' not in new_line:
                     grammer_after_cut.append(new_line)
                     break
-                else: line = new_line
-        else: grammer_after_cut.append(grammer[i])
+                else: 
+                    line = new_line
+        else: 
+            grammer_after_cut.append(grammer[i])
     return grammer_after_cut
 
 # 文法的First集与Follow集，以字典形式存储
 first = {}
 follow = {}
+# 判断字符串是否是非终结符号
+is_vn = lambda x: x.isupper() and len(x) > 1
+# 判断字符串是否是终结符号
+is_vt = lambda x: not is_vn(x)
 
 """
 找到文法中的非终结符vn并为其建立各自的first集和follow集
@@ -51,7 +58,8 @@ def init_first_and_follow(grammer_after_cut):
     for i in range(len(grammer_after_cut)):
         line = grammer_after_cut[i].split('→')
         vn = line[0]
-        if vn not in vns: vns.append(vn)
+        if vn not in vns: 
+            vns.append(vn)
     for i in range(len(vns)):
         first[vns[i]] = []
         follow[vns[i]] = []
@@ -62,25 +70,55 @@ def init_first_and_follow(grammer_after_cut):
 就把它加到产生式左边非终结符号的First集中去
 """
 def first_vt_to_first(grammer_after_cut):
-    global first
-    # 判断字符串是否是终结符号
-    vt = lambda x: not (x.isupper() and len(x) > 1)
+    global first, is_vt
     for i in range(len(grammer_after_cut)):
         line = grammer_after_cut[i]
         index_of_derive = line.find('→')
         vn = line[:index_of_derive]
         first_word = line[index_of_derive + 1:].split(' ')[0]
-        if vt(first_word) and first_word not in first[vn]:
+        if is_vt(first_word) and first_word not in first[vn]:
             first[vn].append(first_word)
-        
-    print(first)
-
                
+"""
+找到字符串中第一个非终结字符，没找到反回False
+"""
+def first_vn_from_line(line):
+    # 得到右侧第一个字符串
+    words_of_right = line.split('→')[1].split(' ')
+    length = len(words_of_right)
+    for i in range(length):
+        if is_vn(words_of_right[i]):
+            return words_of_right[i]
+        elif i == length - 1:
+            return False
+    
+"""
+找到字符串中第二个非终结字符，没找到返回False
+"""
+def second_vn_from_line(line):
+    length = len(line)
+    first_vn = first_vn_from_line(line)
+    if first_vn:
+        index_of_first_vn = line.find(first_vn)
+        length_of_first_vn = len(vn)
+
+        
+    else:
+        return False
+
+"""
+找到字符串中最后一个非终结字符
+"""
+def last_vn_from_line(line):
+    pass
 
 
 def main():
+    '''
     grammer = grammer_from_file()
     grammer_after_cut = grammer_cut(grammer)
     init_first_and_follow(grammer_after_cut)
     first_vt_to_first(grammer_after_cut)
+    '''
+    print(first_vn_from_line("PROGRAM→program identifier ; PARTOFPROGRAM"))
 main()
--- testlamba.py
+++ testlamba.py
@@ -1,76 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-'''
-from io import StringIO
-
-
-_Result = StringIO('')
-_Result.write("a")
-_Result.write("b")
-_Result.write("c")
-_Result.write("d")
-# in fact _Result.write("str" + "\n")
-
-with open('./result.txt', 'at+') as f:
-    f.write(_Result.getvalue())
-    '''
-
-'''
-f = lambda x=2: x**2
-
-print(f(3))
-'''
-'''
-
-with open('./result.txt', 'at+') as f:
-    if f.read() == '':
-        f.write("void")
-'''
-
-'''
-from functools import reduce
-
-DIGITS = {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9}
-
-def char2num(s):
-    return DIGITS[s]
-
-def str2int(s):
-    return reduce(lambda x, y: x * 10 + y, map(char2num, s))
-
-
-print(type(str2int("12345")))
-'''
-'''
-with open('./result.txt', 'r') as f:
-    while True:
-        ch = f.read(1)
-        if ch:
-            print(ch)
-            ch = f.read(1)
-            while ch == '/':
-                f.read(1)
-                if ch == '/':
-                    break
-        else: break
-            '''
-'''
-with open('./result.txt', 'r') as f:
-    ch=f.read(1)
-    while ch:
-        while ch == '/' and f.read(1) == '*':
-            f.read(1)
-            
-    print(i)
-    '''
-'''
-from io import StringIO, BytesIO
-with open('./example.txt', 'rb') as f:
-        _EXAMPLE = BytesIO(f.read())
-_EXAMPLE.seek(-1, 0)
-print((_EXAMPLE.read(1)).decode('utf-8'))
-'''
 
 
 #输入文法求First集和Follow集
@@ -317,3 +247,76 @@ if __name__ == '__main__':
     handleFollow(grammer)#求Follow集
     showFollow()#显示Follow集
     print('____________________________')
+
+'''
+from io import StringIO
+
+
+_Result = StringIO('')
+_Result.write("a")
+_Result.write("b")
+_Result.write("c")
+_Result.write("d")
+# in fact _Result.write("str" + "\n")
+
+with open('./result.txt', 'at+') as f:
+    f.write(_Result.getvalue())
+    '''
+
+'''
+f = lambda x=2: x**2
+
+print(f(3))
+'''
+'''
+
+with open('./result.txt', 'at+') as f:
+    if f.read() == '':
+        f.write("void")
+'''
+
+'''
+from functools import reduce
+
+DIGITS = {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9}
+
+def char2num(s):
+    return DIGITS[s]
+
+def str2int(s):
+    return reduce(lambda x, y: x * 10 + y, map(char2num, s))
+
+
+print(type(str2int("12345")))
+'''
+'''
+with open('./result.txt', 'r') as f:
+    while True:
+        ch = f.read(1)
+        if ch:
+            print(ch)
+            ch = f.read(1)
+            while ch == '/':
+                f.read(1)
+                if ch == '/':
+                    break
+        else: break
+            '''
+'''
+with open('./result.txt', 'r') as f:
+    ch=f.read(1)
+    while ch:
+        while ch == '/' and f.read(1) == '*':
+            f.read(1)
+
+    print(i)
+    '''
+'''
+from io import StringIO, BytesIO
+with open('./example.txt', 'rb') as f:
+        _EXAMPLE = BytesIO(f.read())
+_EXAMPLE.seek(-1, 0)
+print((_EXAMPLE.read(1)).decode('utf-8'))
+'''
+
+
