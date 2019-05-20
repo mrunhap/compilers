a8f7fe19d4182fd8d1b9b30eb1fb8efe91f057f3
mrunhap <liubolovelife@gmail.com>
2019-05-20 10:47:45 +0800

    rename var and chang law · to '

--- compiler.py
+++ compiler.py
@@ -8,92 +8,92 @@ from io import StringIO, BytesIO
 当前目录下的result.txt文件中.
 """
 def scaner(file):
-    DIGITS = {
+    digits = {
         '0': 0, '1': 1, '2': 2, '3': 3, '4': 4,
         '5': 5, '6': 6, '7': 7, '8': 8, '9': 9
         }
     # 获取编码文件并序列化为字典
     with open('./complier.json', 'r') as f:
-        SCANER = json.loads(f.read())
+        code_file = json.loads(f.read())
     # 字节流才能使用seek()改变文件指针pos
     with open(file, 'rb') as f:
-        _EXAMPLE = BytesIO(f.read())
+        program_file = BytesIO(f.read())
     # 每次从file中读取制定字节数，如果文件不存在会自动创建
-    Result = open('./result.txt', 'at+')
+    result_file = open('./result.txt', 'at+')
     # 如果文件不为空则删除文件内容
     if os.path.getsize('./result.txt') > 0:
-        Result.seek(0)
-        Result.truncate()
+        result_file.seek(0)
+        result_file.truncate()
     # result: 内存中用来存放结果的数据流
-    _Result = StringIO('')
-    getCharFromFile = lambda size=1: (_EXAMPLE.read(size)).decode('utf-8')
-    buildFormatStr = lambda key, token: "(%d, %s)\n"%(SCANER[key], token)
+    char2memory = StringIO('')
+    char_from_file = lambda size=1: (program_file.read(size)).decode('utf-8')
+    format_string = lambda key, token: "(%d, %s)\n"%(code_file[key], token)
 
-    ch = getCharFromFile()
+    ch = char_from_file()
     while ch:
         # token: 用来连接读取到的字符
         token = ''
         while ch in [' ', '\n', '\t']:
-            ch = getCharFromFile()
+            ch = char_from_file()
         # 是否以字母开始或全是字母
         if ch.isalpha():
             token += ch
-            ch = getCharFromFile()
+            ch = char_from_file()
             # 是否全是字母或数字
-            while ch.isalpha() or ch in DIGITS:
+            while ch.isalpha() or ch in digits:
                 token += ch
-                ch = getCharFromFile()
+                ch = char_from_file()
             # 如果不在编码文件中，则为用户自定义标志符，否则为保留字
-            if token not in SCANER:
-                _Result.write(buildFormatStr("ID", token))
+            if token not in code_file:
+                char2memory.write(format_string("ID", token))
             else:
-                _Result.write(buildFormatStr(token, ''))
-        elif ch in DIGITS:
+                char2memory.write(format_string(token, ''))
+        elif ch in digits:
             token += ch
-            ch = getCharFromFile()
+            ch = char_from_file()
             # TODO: 判断负数与小数
-            while ch in DIGITS:
+            while ch in digits:
                 token += ch
-                ch = getCharFromFile()
+                ch = char_from_file()
             # 将字符串转换为二进制存储，二进制以0b开头，故使用[2:]
-            _Result.write(buildFormatStr("NUM", bin(int(token))[2:]))
+            char2memory.write(format_string("NUM", bin(int(token))[2:]))
         elif ch in [',', ';', '+', '-', '*', '=', '.']:
-            _Result.write(buildFormatStr(ch, ch))
-            ch = getCharFromFile()
+            char2memory.write(format_string(ch, ch))
+            ch = char_from_file()
         elif ch in ['>', '<', ':']:
             token += ch
-            # 使用_ch代替ch进行判断，否则SCANER[ch]会发生KeyError:ch
-            _ch = getCharFromFile()
+            # 使用_ch代替ch进行判断，否则code_file[ch]会发生KeyError:ch
+            _ch = char_from_file()
             if _ch == '=':
                 token += _ch
-                _Result.write(buildFormatStr(token, token))
-                ch = getCharFromFile()
+                char2memory.write(format_string(token, token))
+                ch = char_from_file()
             else:
-                _EXAMPLE.seek(-1, 1)
-                _Result.write(buildFormatStr(ch, ch))
-                ch = getCharFromFile()
+                program_file.seek(-1, 1)
+                char2memory.write(format_string(ch, ch))
+                ch = char_from_file()
         elif ch == '/':
-            _ch = getCharFromFile()
+            _ch = char_from_file()
             if _ch == '/':
-                _EXAMPLE.readline()
-                ch = getCharFromFile()
-            elif _ch == '*': 
+                program_file.readline()
+                ch = char_from_file()
+            elif _ch == '*':
                 while _ch:
-                    if getCharFromFile() == '*' and getCharFromFile() == '/':
-                        ch = getCharFromFile()
+                    if char_from_file() == '*' and char_from_file() == '/':
+                        ch = char_from_file()
                         break
             else:
-                _EXAMPLE.seek(-1, 1)
-                _Result.write(buildFormatStr(ch, ch))
-                ch = getCharFromFile()
-        else: 
+                program_file.seek(-1, 1)
+                char2memory.write(format_string(ch, ch))
+                ch = char_from_file()
+        else:
             print("unknown symbol %s"%ch)
             break
-    Result.write(_Result.getvalue())
-    Result.close()
+    result_file.write(char2memory.getvalue())
+    result_file.close()
 
 def main():
-    if len(sys.argv) == 1: 
+    if len(sys.argv) == 1:
         file = input("请输入待分析文件路径：")
         scaner(file)
     elif (sys.argv[1]) and (len(sys.argv) == 2):
--- test.py
+++ test.py
@@ -2,13 +2,13 @@
 # -*- coding: utf-8 -*-
 from io import StringIO
 
-def getGrammer():
+def grammer_from_file():
     # 声明一个文法列表，用来保存各个文法的产生式子
     grammer = []
     with open('./law.english.txt', 'r') as f:
-        grammerFile = StringIO(f.read())
+        grammer_file = StringIO(f.read())
     while True:
-        line = grammerFile.readline()
+        line = grammer_file.readline()
         if line:
             # 去掉末尾\n
             grammer.append(line[:-1])
@@ -17,40 +17,47 @@ def getGrammer():
             break
     return grammer
 # 把包含选择运算符的产生式分为两个
-def cutGrammer(grammer):
-    j = 1
-    grammerAfterCut = []
+def grammer_cut(grammer):
+    grammer_after_cut = []
     for i in range(len(grammer)):
         line = grammer[i]
         if '|' in line:
             while True:
-                print("line\t:\t%s"%line)
-                index = line.find('|')
-                print("index\t:\t%s"%index)
-                print("line[:index]\t:\t%s"%line[:index])
-                grammerAfterCut.append(line[:index])
-                index1 = line.find('→')
-                print("index1\t:\t%s"%index1)
-                print("line[:index1 + 1]\t:\t%s"%line[:index1 + 1])
-                print("line[index + 1:]\t:\t%s"%line[index + 1:])
-                line1 = line[:index1 + 1] + line[index + 1:]
-                print("line1\t:\t%s"%line1)
-                print("j:%d\n\n"%j)
-                j += 1
-                if '|' not in line:
-                    # TODO: 故意写错，只运行一次循环
-                    grammerAfterCut.append[line1]
+                index_of_or = line.find('|')
+                grammer_after_cut.append(line[:index])
+                index_of_derive = line.find('→')
+                new_line = line[:index_of_derive + 1] + line[index_of_or + 1:]
+                if '|' not in new_line:
+                    grammer_after_cut.append(new_line)
                     break
                 else:
-                    line = line1
+                    line = new_line
         else:
-            grammerAfterCut.append(grammer[i])
-    return grammerAfterCut
+            grammer_after_cut.append(grammer[i])
+    return grammer_after_cut
 
 # 文法的First集与Follow集，以字典形式存储
-First = {}
-Follow = {}
+first = {}
+follow = {}
+
+# 找到文法中的非终结符vn并为其建立各自的first集和follow集
+def init_first_and_follow(grammer):
+    vns = []
+    for i in range(len(grammer)):
+        line = grammer[i]
+        for j in range(len(line)):
+            if line[j] >= 'A' and line[j] <= 'Z':
+                if j < len(line) - 1 and line[j + 1] == "'":
+                    vn = line[j] + "'"
+                    if vn not in vns:
+                        vns.append(vn)
+                    else:
+                        vn = line[j] + ''
+                        if vn not in vns:
+                            vns.append(vn)
+
+
 def main():
-    grammer = getGrammer()
-    print(cutGrammer(grammer))
+    grammer = grammer_from_file()
+    print(grammer_cut(grammer))
 main()
