79452c4adfa52127640730db1886ca31b8738e81
mrunhap <liubolovelife@gmail.com>
2019-05-21 12:14:18 +0800

    first almost done

--- test.py
+++ test.py
@@ -12,7 +12,6 @@ is_vt = lambda x: not is_vn(x)
 # 计算列表中非终结字符的数量
 count_vn = lambda list_of_right: len(list(filter(is_vn, list_of_right)))
 
-
 """
 从文件中获得文法，成功则返回一个list，其中每个元素为一行产生式.
 """
@@ -26,7 +25,7 @@ def grammer_from_file():
         if line:
             # 去掉末尾\n
             grammer.append(line[:-1])
-        else: 
+        else:
             break
     return grammer
 
@@ -47,9 +46,9 @@ def grammer_cut(grammer):
                 if '|' not in new_line:
                     grammer_after_cut.append(new_line)
                     break
-                else: 
+                else:
                     line = new_line
-        else: 
+        else:
             grammer_after_cut.append(grammer[i])
     return grammer_after_cut
 
@@ -63,7 +62,7 @@ def init_first_and_follow(grammer_after_cut):
     for i in range(len(grammer_after_cut)):
         line = grammer_after_cut[i].split('→')
         vn = line[0]
-        if vn not in vns: 
+        if vn not in vns:
             vns.append(vn)
     for i in range(len(vns)):
         first[vns[i]] = []
@@ -86,7 +85,7 @@ def first_vn_from_line(line):
             return list_of_right[i]
         elif i == length - 1:
             return False
-    
+
 """
 找到字符串中第二个非终结字符，没找到返回False.
 line: String, 文法中的一行产生式.
@@ -107,7 +106,7 @@ line: String, 文法中的一行产生式.
 def last_vn_from_line(line):
     list_of_right = line.split('→')[1].split(' ')
     # list_of_right 必须是一个list
-    count = count_vn(list_of_right)   
+    count = count_vn(list_of_right)
     if count == 0: return False
     elif count == 1: return first_vn_from_line(line)
     elif count == 2: return second_vn_from_line(line)
@@ -115,7 +114,7 @@ def last_vn_from_line(line):
         for i in range(len(list_of_right)):
             if is_vn(list_of_right[-i]):
                 return list_of_right[-i]
-        
+
 """
 扫描文法中每一个产生式，如果右边第一个符号是一个非终结符号，
 就把它加到产生式左边非终结符号的First集中去.
@@ -130,7 +129,7 @@ def first_vt_to_first(grammer_after_cut):
         first_word = line[index_of_derive + 1:].split(' ')[0]
         if is_vt(first_word) and first_word not in first[vn]:
             first[vn].append(first_word)
-               
+
 """
 扫描文法中的每一个产生式，对于产生式右边第一个符号不是非终结符的情况，
 把右边非终结符First集中的元素加入到左边非终结符的First集中去,
@@ -142,30 +141,44 @@ def first_not_vt(grammer_after_cut):
     global first
     for i in range(len(grammer_after_cut)):
         line = grammer_after_cut[i]
-        index_of_derive = line.find('→')
-        list_of_right = line.split('→')[1].split(' ')
-        if is_vt(list_of_right[i]):
+        line_cut = line.split('→')
+        vn_of_left = line_cut[0]
+        part_of_right = line_cut[1]
+        list_of_right = part_of_right.split(' ')
+
+        if is_vt(list_of_right[0]):
             continue
-        first_vn = first_vn_from_line(line)
-        second_vn = second_vn_from_line(line)
-        flag = 1
-        while flag == 1:
-            for 
+        
+        print()
+    '''
+    for i in range(len(grammer_after_cut)):
+        # 右侧第一个为终结符，已经添加到左侧非终结符中
+        if is_vt(list_of_right[0]):
+            continue
+        first_vn = first_vn_from_line(grammer_after_cut[i])
+        print(first_vn)
+        for j in range(len(first[first_vn])):
+            if first[first_vn][j] not in first[vn_of_left]:
+                first[vn_of_left].append(first[first_vn][i])
+            elif 'ε' in first[list_of_right[0]]:
+                second_vn = second_vn_from_line(grammer_after_cut[i])
+                if second_vn not in first[vn_of_left]:
+                    first[vn_of_left].extend(first[second_vn])
+                    '''
 
 """
 构造first集.
 grammer: list, 文法列表，每个元素为文法的一行产生式.
 """
-def first_property(grammer):
-    grammer_after_cut = grammer_cut(grammer)
-    init_first_and_follow()
+def first_property(grammer_after_cut):
     first_vt_to_first(grammer_after_cut)
     first_not_vt(grammer_after_cut)
     first_not_vt(grammer_after_cut)
 
 def main():
-    '''
     grammer = grammer_from_file()
-    '''
-    print(last_vn_from_line("PROGRAM→program IDENTIFIER ; PARTOFPROGRAM ASDSADAD"))
+    grammer_after_cut = grammer_cut(grammer)
+    init_first_and_follow(grammer_after_cut)
+    first_vt_to_first(grammer_after_cut)
+    first_not_vt(grammer_after_cut)
 main()
